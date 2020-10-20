52a810b2c6a5c55b8c680dab1a2a8d9f1d4ce509
DevroDev <devroshein.dev@gmail.com>
2020-10-20 07:40:28 +0630

    php-simple-auth-example

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$id=$_POST['id'];
+$password=$_POST['password'];
+if($id=='admin'&& $password=="123"){
+    $_SESSION['auth']=true;
+    $_SESSION['id']=$id;
+}
+elseif($id=='client'&&$password=="321"){
+    $_SESSION['auth']=true;
+    $_SESSION['id']="Client";
+}
+header("location:index.php");
+?>
